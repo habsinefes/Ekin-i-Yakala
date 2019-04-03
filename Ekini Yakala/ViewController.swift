30daf3a7ce985f174c01db0dc8758fd0ca3d20cd
can <habsinefes@gmail.com>
2019-04-03 15:32:53 +0300

    Initial Commit

---
+++ Ekini Yakala/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ekini Yakala
+//
+//  Created by Can Ates on 3.04.2019.
+//  Copyright © 2019 Can Ates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
